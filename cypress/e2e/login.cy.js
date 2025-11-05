8aa656e484b9327c621f556fef4b93148de34ffb
EmillySRamalho <emilly_bsfpb@hotmail.com>
2025-11-05 20:14:29 -0300

    comentarios

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,13 +5,20 @@ describe('Preencher o formulário de teste Cypress', () => {
   })
 
   it('Preencher o formulário', () => {
+    // Preencher o campo de nome 
     cy.get('input[name="nome"]').type('Emilly Ramalho')
+
+    
     cy.get('input[name="email"]').type('emilly.ramalho@example.com')
     cy.get('input[name="senha"]').type('SenhaSegura123!')
     cy.get('#idade').type('25')
     cy.get('#data').type('2000-02-25')
     cy.get('#telefone').type('11987654321')
     cy.get('#url').type('https://example.com')
+
+   // adicionar imagem, nesse caso você deve ter a imagem "foto.jpg" na pasta cypress/fixtures
+    cy.get('#arquivo').attachFile('foto.jpg');
+
 
   })
 
