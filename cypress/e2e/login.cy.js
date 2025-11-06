ad2b26f70113a3c7e134db9f922701d5c83694d3
EmillySRamalho <emilly_bsfpb@hotmail.com>
2025-11-06 20:50:14 -0300

    novos testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -41,6 +41,21 @@ describe('Preencher o formulário de teste Cypress', () => {
     cy.get('select[name="pais"]').select('Brasil');
 
     cy.get('select[name="pais"]').select('Estados Unidos');
+
+    // Comentário
+
+   cy.get('textarea[name="comentario"]').type(`Este é um comentário de teste para o formulário Cypress.`);
+
+   cy.get(`textarea[name="comentario"]`).clear();
+   
+   cy.get('textarea[name="comentario"]').type(`Este é apenas um texto genérico usado para testes automatizados. O objetivo é verificar se o campo aceita entradas válidas e se o comportamento do formulário está conforme o esperado`);
+
+    // Ajustar o nível de satisfação
+    cy.get('#satisfacao').invoke('val', 5).trigger('input');
+
+    
+
+    cy.get('#termos').check();
   })
 
-})
+});
