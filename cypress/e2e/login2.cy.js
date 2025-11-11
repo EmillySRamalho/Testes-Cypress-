31a00d180d9f4fc55dda769f57249953a0c0eefd
EmillySRamalho <emilly_bsfpb@hotmail.com>
2025-11-10 21:12:07 -0300

    adicao de senha aleatoria

--- cypress/e2e/login2.cy.js
+++ cypress/e2e/login2.cy.js
@@ -5,13 +5,19 @@ describe('Preencher o formulário de teste Cypress', () => {
   })
 
   // Teste para preencher e enviar o formulário com dados variados, nesse caso ele pega esses nomes que estão disponíveis em um array e escolhe um aleatoriamente.
+
  it('Preenche e envia o formulário com sucesso', () => {
     const nomes = ['Ana Souza', 'Lucas Almeida', 'Mariana Costa', 'Pedro Silva', 'Carla Ribeiro', 'João Santos', 'Beatriz Fernandes', 'Rafael Oliveira', 'Juliana Lima', 'Gabriel Pereira', 'Camila Gomes', 'Felipe Rodrigues', 'Larissa Martins', 'Bruno Carvalho', 'Amanda Dias', 'Thiago Mendes', 'Sofia Araújo', 'Gustavo Nunes', 'Isabela Rocha', 'Diego Cunha']
     const nome = nomes[Math.floor(Math.random() * nomes.length)]
     cy.get('#nome').type(nome)
 
-    const email = nome.toLowerCase().replace(' ', '.') + '@example.com'
+    // Gerar email baseado no nome selecionado, substituindo espaços por pontos, tudo em minúsculo.
+    const email = nome.toLowerCase().replace(/ /g, '.') + '@example.com'
     cy.get('#email').type(email)
+
+    // Preencher o campo de senha
+     const senha = `senha${Math.floor(Math.random() * 1000)}`
+    cy.get('#senha').type(senha)
 
 
 })
